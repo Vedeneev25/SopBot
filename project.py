b8e9f7bf3f2415ef887e77cd2e971edae2f0bc74
vedeneev25 <vedeveev@gmail.com>
2022-11-15 21:39:00 +0300

    В код добавил комментарии. Добавлено небольшое улучшение, возврат

--- project.py
+++ project.py
@@ -8,17 +8,16 @@ import xlwt
 from openpyxl import Workbook
 from openpyxl import load_workbook
 
-
 token = 
-
 bot = telebot.TeleBot(token)
 
+#Работа с ексель файлом
 links = openpyxl.load_workbook(("Ссылки.xlsx"))
 sheet1 = links['list1']
 maxRow = sheet1.max_row
-#Определяем все авиакомпании какие есть
 print(maxRow)
 
+#Определяем все авиакомпании какие есть в файле. Функция возвращает список
 def Airlines (sheet1, maxRow):
     airlines = []
     i = 1
@@ -32,7 +31,7 @@ def Airlines (sheet1, maxRow):
     #print (airlines)
     return airlines
 
-#Определяем категории
+#Определяем категории. Функция принимает авиакомпанию, и возвращает список возможных ответов.
 def Categorii(Airlinename, maxRow, sheet1):
     #c = input("Выбери АК: ")
     category = []
@@ -47,28 +46,36 @@ def Categorii(Airlinename, maxRow, sheet1):
                 i += 1
         else: 
             i +=1
-        ##print (category)
     return category
 
-#print(Categorii('Аэрофлот', maxRow, sheet1))
-#print(Airlines(sheet1, maxRow))
+#Работа с ботом
 
 AirlineName = ''
 CategoryName = ''
 
-@bot.message_handler(commands=['start'])
-def start_message(message):
-    bot.send_message(message.from_user.id, 'Привет! Я бот СОПП. Выбери авиакомпанию', reply_markup=keyboard2)
-    bot.register_next_step_handler(message, category)
+# Say Hello to Bot
+keyboard3 = telebot.types.ReplyKeyboardMarkup(True, True)
+keyboard3.row('Привет! ')
 
-
+#Формируем клавиатуру для первого сообщения, которую будем использовать в стартовом сообщении.
 keyboard2 = telebot.types.ReplyKeyboardMarkup(True, True)
 keyboard2.row('')
 i = 0
 while i < len(Airlines(sheet1, maxRow)):
         keyboard2.add(telebot.types.InlineKeyboardButton(text=Airlines(sheet1, maxRow)[i]))
         i = i + 1
- 
+
+@bot.message_handler(commands=['start'])
+def start_message(message):
+    bot.send_message(message.from_user.id, 'Привет, я бот СОПП', reply_markup=keyboard3)
+
+
+@bot.message_handler(content_types=['text'])
+def Get_airline(message):
+    bot.send_message(message.from_user.id, 'Выбери авиакомпанию', reply_markup=keyboard2)
+    bot.register_next_step_handler(message, category)
+
+#На входе у метода уже есть название авиакомпании. Формируется список и клавиатура из всех возможных ответов. 
 def category(message):
     global AirlineName
     AirlineName = message.text
@@ -78,58 +85,30 @@ def category(message):
     while i < len(Categorii(AirlineName, maxRow, sheet1)):
         keyboard1.add(telebot.types.InlineKeyboardButton(text=Categorii(message, maxRow, sheet1)[i]))
         i = i + 1
+    keyboard1.add(telebot.types.InlineKeyboardButton(text="Другая авиакомпания"))
     bot.send_message(message.from_user.id, 'Так, а теперь выбери категорию: ', reply_markup=keyboard1)
     bot.register_next_step_handler(message, send_text)
 
 
-
-#@bot.message_handler(content_types=['text'])
 def send_text(message):
     global CategoryName
     CategoryName = message.text
-    i = 1
-    while (i < maxRow):
-        if (AirlineName == sheet1.cell(row = i, column = 1).value and CategoryName == sheet1.cell(row = i, column = 2).value):
-            answer = sheet1.cell(row = i, column = 3).value
-            break
-        else:
-            i += 1
-    bot.send_message(message.from_user.id, "вот ссылка на ответ: " + answer)
+    if CategoryName == "Другая авиакомпания":
+        bot.send_message(message.from_user.id, 'Перевыбери авиакомпанию', reply_markup=keyboard2)
+        bot.register_next_step_handler(message, category)     
+    else:
+        i=1
+        while (i < maxRow):
+            if (AirlineName == sheet1.cell(row = i, column = 1).value and CategoryName == sheet1.cell(row = i, column = 2).value):
+                answer = sheet1.cell(row = i, column = 3).value
+                break
+            else:
+                i += 1
+        bot.send_message(message.from_user.id, "вот ссылка на ответ: " + answer)
+        bot.send_message(message.from_user.id, 'Выбери авиакомпанию', reply_markup=keyboard2)
+        bot.register_next_step_handler(message, category)  
         
         
-    
-
-
-
-
-
 bot.polling()
 
-# def Airlines(message):
-#     markup = telebot.types.InlineKeyboardMarkup()
-#     markup.add(telebot.types.InlineKeyboardButton(text='Аэрофлот', callback_data=3))
-#     bot.send_message(message.chat.id, text="выбери авиакомпанию", reply_markup=markup)
 
-# @bot.message_handler(content_types=['text'])
-# def send_text(message):
-#     if message.text.lower() == 'привет':
-#         bot.send_message(message.chat.id, 'Привет! ззз')
-#         Airlines(message)
-
-# @bot.callback_query_handler(func=lambda call: True)
-# def query_handler(call,):
-#     bot.answer_callback_query(callback_query_id=call.id, text='Приятного времени ожидания!')                                                                                       
-#     answer = ''
-#     if (call.data == sheet1.cell(row = i, column = 2).value and message.text == sheet1.cell(row = i, column = 1).value):
-#         answer = 'link1'
-#         bot.send_message(call.message.chat.id, answer, reply_markup=keyboard2)
-
-
-
-# def AirlinesTG(message):
-#     markup = telebot.types.InlineKeyboardMarkup()
-#     i = 0
-#     while i < len(Airlines(sheet1, maxRow)):
-#         markup.add(telebot.types.InlineKeyboardButton(text=Airlines(sheet1, maxRow)[i]))
-#         i = i + 1
-#     bot.send_message(message.chat.id, 'Выбери авиакомпанию: ', reply_markup=keyboard2)
