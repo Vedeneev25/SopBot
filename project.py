7f5ebbb02d8c9a0924dd40ef20801c1cf3508045
vedeneev25 <vedeveev@gmail.com>
2022-11-15 20:42:27 +0300

    бот заработал. Ссылки отправляются

--- project.py
+++ project.py
@@ -9,7 +9,7 @@ from openpyxl import Workbook
 from openpyxl import load_workbook
 
 
-token = '
+token = 
 
 bot = telebot.TeleBot(token)
 
@@ -33,13 +33,13 @@ def Airlines (sheet1, maxRow):
     return airlines
 
 #Определяем категории
-def Categorii(message, maxRow, sheet1):
+def Categorii(Airlinename, maxRow, sheet1):
     #c = input("Выбери АК: ")
     category = []
     i = 1
     while i < maxRow:
         cat = sheet1.cell(row = i, column = 2).value
-        if message.text == sheet1.cell(row = i, column = 1).value:
+        if AirlineName == sheet1.cell(row = i, column = 1).value:
             if (cat in category):
                 i = i + 1
             else:
@@ -53,6 +53,13 @@ def Categorii(message, maxRow, sheet1):
 #print(Categorii('Аэрофлот', maxRow, sheet1))
 #print(Airlines(sheet1, maxRow))
 
+AirlineName = ''
+CategoryName = ''
+
+@bot.message_handler(commands=['start'])
+def start_message(message):
+    bot.send_message(message.from_user.id, 'Привет! Я бот СОПП. Выбери авиакомпанию', reply_markup=keyboard2)
+    bot.register_next_step_handler(message, category)
 
 
 keyboard2 = telebot.types.ReplyKeyboardMarkup(True, True)
@@ -63,22 +70,41 @@ while i < len(Airlines(sheet1, maxRow)):
         i = i + 1
  
 def category(message):
-    markup = telebot.types.InlineKeyboardMarkup()
+    global AirlineName
+    AirlineName = message.text
+    keyboard1 = telebot.types.ReplyKeyboardMarkup(True, True)
+    keyboard1.row('')
     i = 0
-    while i < len(Categorii(message, maxRow, sheet1)):
-        markup.add(telebot.types.InlineKeyboardButton(text=Categorii(message, maxRow, sheet1)[i], callback_data=i+1))
+    while i < len(Categorii(AirlineName, maxRow, sheet1)):
+        keyboard1.add(telebot.types.InlineKeyboardButton(text=Categorii(message, maxRow, sheet1)[i]))
         i = i + 1
-    bot.send_message(message.chat.id, 'Так, а теперь выбери категорию: ', reply_markup=markup)
-    print(message.text)
-    print(Categorii(message, maxRow, sheet1))
+    bot.send_message(message.from_user.id, 'Так, а теперь выбери категорию: ', reply_markup=keyboard1)
+    bot.register_next_step_handler(message, send_text)
 
-@bot.message_handler(commands=['start'])
-def start_message(message):
-    bot.send_message(message.chat.id, 'Привет! Я бот СОПП.', reply_markup=keyboard2)
 
-@bot.message_handler(content_types=['text'])
+
+#@bot.message_handler(content_types=['text'])
 def send_text(message):
-    category (message)
+    global CategoryName
+    CategoryName = message.text
+    i = 1
+    while (i < maxRow):
+        if (AirlineName == sheet1.cell(row = i, column = 1).value and CategoryName == sheet1.cell(row = i, column = 2).value):
+            answer = sheet1.cell(row = i, column = 3).value
+            break
+        else:
+            i += 1
+    bot.send_message(message.from_user.id, "вот ссылка на ответ: " + answer)
+        
+        
+    
+
+
+
+
+
+bot.polling()
+
 # def Airlines(message):
 #     markup = telebot.types.InlineKeyboardMarkup()
 #     markup.add(telebot.types.InlineKeyboardButton(text='Аэрофлот', callback_data=3))
@@ -90,20 +116,13 @@ def send_text(message):
 #         bot.send_message(message.chat.id, 'Привет! ззз')
 #         Airlines(message)
 
-@bot.callback_query_handler(func=lambda call: True)
-
-def query_handler(call):
-    bot.answer_callback_query(callback_query_id=call.id, text='Приятного времени ожидания!')                                                                                       
-    answer = ''
-    if call.data == '3': #AFL
-        answer = 'link1'
-
-
-
-
-bot.polling()
-
-
+# @bot.callback_query_handler(func=lambda call: True)
+# def query_handler(call,):
+#     bot.answer_callback_query(callback_query_id=call.id, text='Приятного времени ожидания!')                                                                                       
+#     answer = ''
+#     if (call.data == sheet1.cell(row = i, column = 2).value and message.text == sheet1.cell(row = i, column = 1).value):
+#         answer = 'link1'
+#         bot.send_message(call.message.chat.id, answer, reply_markup=keyboard2)
 
 
 
